551eb6304c8dd08b31f15c0b249a446c99acdcbc
steven cain <stevencainn@gmail.com>
2019-11-19 19:08:16 -0500

    added click function

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+var friendsArray = [{
+    "name": "Ahmed",
+    "photo": "example",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+},{
+    "name": "Steven",
+    "photo": "example",
+    "scores": [4, 5, 2, 3, 5, 1, 2, 4, 3, 1]
+},{
+    "name": "Bryan",
+    "photo": "example",
+    "scores": [2, 1, 3, 5, 4, 2, 2, 4, 4, 1]
+}];
+
+module.exports = friendsArray;
